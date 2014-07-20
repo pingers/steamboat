e3eb23fe4f4a8a8c80a6382caf4d4a32a4eb5927
pingers <pingers@gmail.com>
2014-07-20 12:22:20 +0930

    Add index page with a form to collect user name.

--- src/SteamBoat/SteamBoatBundle/Controller/DefaultController.php
+++ src/SteamBoat/SteamBoatBundle/Controller/DefaultController.php
@@ -2,18 +2,41 @@
 
 namespace SteamBoat\SteamBoatBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use SteamId;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->add('name', 'text')
+            ->add('Go', 'submit')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $data = $form->getData();
+            return $this->redirect($this->generateUrl(
+                'steam_boat_list_games',
+                array('name' => $data['name'])
+            ));
+        }
+
+        return $this->render('SteamBoatBundle:Default:index.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    public function listGamesAction($name)
     {
         $repository = $this->getDoctrine()
           ->getRepository('SteamBoatBundle:SteamIdStorage');
         $steamIdStorage = $repository->findOneByNickname($name);
 
-        return $this->render('SteamBoatBundle:Default:index.html.twig', array(
+        return $this->render('SteamBoatBundle:Default:listGames.html.twig', array(
             'id' => $steamIdStorage,
             'games' => $steamIdStorage->getGames(),
          ));
